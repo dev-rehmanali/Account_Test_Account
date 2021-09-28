d9b404ce3e5fea2b1b09a1048116e9f57aa16d4b
dev.rehmanali <82761490+dev-rehmanali@users.noreply.github.com>
2021-09-28 15:42:32 +0500

    Added account and testaccount files

---
+++ Account.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task3._2
+{
+    class Account
+    {
+        private decimal balance;
+        private string name;
+        
+        public string Name 
+        {
+
+            get 
+            {
+                return name;
+            }
+        }
+
+        public Account(string name, decimal balance) {
+
+            this.name = name;
+            this.balance = balance;
+        }
+
+        public bool Deposit(decimal amount) {
+
+            if (amount <= 0)
+            {
+                Console.WriteLine("Could not deposit amount");
+                return false;
+            }
+            else { 
+            
+                this.balance += amount;
+                Console.WriteLine("Successfully Deposited");
+                Console.WriteLine("New Balance: " + this.balance);
+                return true;
+            }
+
+        }//Deposit
+
+        public bool Withdraw(decimal amount) {
+
+            if (amount < 0 || amount > this.balance)
+            {
+
+                return false;
+            }
+            else {
+
+                this.balance -= amount;
+                Console.WriteLine("Successfull Withdrawl");
+                Console.WriteLine("New Balance: " + this.balance);
+                return true;
+            }
+
+        }//Withdraw
+
+        public void Print() {
+
+            Console.WriteLine("Account Name is: " + this.name);
+            Console.WriteLine("Account Balance is: " + this.balance);
+        }
+
+
+    }
+}
---
+++ TestAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Task3._2;
+
+namespace Task2._2
+{
+    class TestAccount
+    {
+        static void Main(string[] args)
+        {
+
+            Account account = new Account("AC-B01",85000);
+
+            account.Deposit(5000);
+
+            account.Withdraw(25000);
+
+            account.Print();
+        }
+    }
+}
